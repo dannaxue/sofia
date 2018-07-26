03c8e197fbead296ebf9f56ca33cc9d09587b693
dannaxue <dannaxue@stanford.edu>
2018-07-26 11:01:13 -0700

    edited plot toolbar, took out unnecessary buttons and moved plot button to the toolbar. removed previous button

--- sofia/plots.py
+++ sofia/plots.py
@@ -1,8 +1,12 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QPushButton
+import os
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout
 from matplotlib.backends.backend_qt5agg import FigureCanvas
-from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT 
+# as NavigationToolbar
 from matplotlib.figure import Figure
 from sofia.plotter import plotImage, plotContour
+from PyQt5.QtWidgets import QSplitter
+from PyQt5.QtCore import Qt
 
 class PLOT_UI(QWidget):
     
@@ -18,8 +22,11 @@ class PLOT_UI(QWidget):
         layout = QHBoxLayout()
         self.plot1 = IMAGE_PLOT()
         self.plot2 = CONTOUR_PLOT()
-        layout.addWidget(self.plot1)
-        layout.addWidget(self.plot2)
+        splitter = QSplitter(Qt.Horizontal)
+        splitter.addWidget(self.plot1)
+        splitter.addWidget(self.plot2)
+        splitter.setStretchFactor(1, 1)
+        layout.addWidget(splitter)
         self.setLayout(layout)
 
 class IMAGE_PLOT(QWidget):
@@ -38,14 +45,14 @@ class IMAGE_PLOT(QWidget):
         self.toolbar = NavigationToolbar(self.canvas, self)
 
         # Creates a button1
-        self.button = QPushButton('Plot')
-        self.button.clicked.connect(self.plot)
+        # self.button = QPushButton('Plot')
+        # self.button.clicked.connect(self.plot)
         
         # Sets up layout
         layout = QVBoxLayout()
         layout.addWidget(self.canvas)
         layout.addWidget(self.toolbar)
-        layout.addWidget(self.button)
+        # layout.addWidget(self.button)
         self.setLayout(layout)
     
     # Plots data
@@ -68,18 +75,29 @@ class CONTOUR_PLOT(QWidget):
         # Creates a Canvas and Navigation Toolbar
         self.canvas = FigureCanvas(self.figure)
         self.toolbar = NavigationToolbar(self.canvas, self)
-
-        # Creates a button1
-        self.button = QPushButton('Plot')
-        self.button.clicked.connect(self.plot)
         
         # Sets up layout
         layout = QVBoxLayout()
         layout.addWidget(self.canvas)
         layout.addWidget(self.toolbar)
-        layout.addWidget(self.button)
+        # layout.addWidget(self.button)
         self.setLayout(layout)
     
     # Plots data
     def plot(self):
         plotContour(self)
+        
+class NavigationToolbar(NavigationToolbar2QT):
+    def __init__(self,canvas,parent):
+        # Select only a few buttons
+        self.iconDir = os.path.join(os.path.dirname(os.path.abspath(__file__)),"icons")
+        self.toolitems = [
+            ('Home','Go back to original limits','home','home'),
+            ('Pan','Pan figure','move','pan'),
+            ('Zoom','Zoom in','zoom_to_rect','zoom'),
+            ('Save', 'Save the figure', 'filesave', 'save_figure'),
+            # ('Plot', 'plot', 'plot', 'plot'),
+        ]
+        self.parent = parent
+        super().__init__(canvas,parent)
+        self.addAction(self._icon('plot.png'), 'Plot', self.parent.plot)
